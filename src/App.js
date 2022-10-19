eb96cbf5dfc8b7fdd6dcf49c915adead96b5ffae
unknown <veertyagia@mail.com>
2022-10-19 15:10:13 +0530

    pages route in web app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgetPassword from "./pages/ForgetPassword";
+import SignIn from "./pages/SignIn";
 function App() {
   return (
-    <div className="text-2xl">
-    <h1>hello</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignIn />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
- 
---
+++ src/pages/Offers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Offers = () => {
+  return (
+    <div>r</div>
+  )
+}
+
+export default Offers;
